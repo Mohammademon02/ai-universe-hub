936212f27c718d4df66a99658b72a1200779a79d
mohammademon02 <mohammademon0207@gmail.com>
2023-03-04 03:55:59 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -46,11 +46,85 @@ const loadCardDetails = id => {
     const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => displayCardDetails(data))
+    .then(data => displayCardDetails(data.data))
 }
 
 const displayCardDetails = card => {
-    
+
+    const modalContainer = document.getElementById('modal-container')
+    console.log(card);
+    const modalDiv = document.createElement('div');
+    modalDiv.innerHTML = `
+    <div class="modal fade" id="cardModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="cardModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="cardModalLabel">Modal title</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="d-flex">
+
+                  <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">${card.description}</h5>
+
+                      <div class="d-flex justify-content-around">
+                        <div>
+                          <p>price</p>
+                          <p>Basic</p>
+                        </div>
+                        <div>
+                          <p>price</p>
+                          <p>Basic</p>
+                        </div>
+                        <div>
+                          <p>price</p>
+                          <p>Basic</p>
+                        </div>
+                      </div>
+
+                      <div class="d-flex justify-content-around">
+                        <div>
+                          <h5 class="card-title">Features</h5>
+                          <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                          </ul>
+                        </div>
+  
+                        <div>
+                          <h5 class="card-title">Integrations</h5>
+                          <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                          </ul>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+
+                  <div class="card" style="width: 18rem;">
+                    <div class="">
+                      <img src=" " class="card-img-top" alt="...">
+                      <button class="btn btn-danger"></button>
+                    </div>
+                    <div class="card-body">
+                      <h3>Hi, how are you doing today?</h3>
+                      <p> </p>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+    `;
+    modalContainer.appendChild(modalDiv);
 }
 loadCards();
 
