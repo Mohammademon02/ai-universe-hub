3e59d253ae537d0947c2b65510bdfc8f1d81719d
mohammademon02 <mohammademon0207@gmail.com>
2023-03-04 23:15:10 +0600

    see more button added

--- js/app.js
+++ js/app.js
@@ -10,10 +10,9 @@ const loadCards = () => {
 const displayCards = cards => {
     // console.log(cards);
 
-    // // // start spinner 
-    //   toggleSpinner(true);
-
     const cardsContainer = document.getElementById('cards-container');
+
+      cards = cards.slice(0,6);
     cards.forEach(card => {
         // console.log(card.features)
         const cardDiv = document.createElement('div')
@@ -45,8 +44,57 @@ const displayCards = cards => {
 
     // start spinner 
     toggleSpinner(false);
-
 }
+
+const lodeMore = document.getElementById('lode-more');
+  lodeMore.addEventListener('click', function () {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCards(data.data.tools));
+
+    const displayCards = cards => {
+      // console.log(cards);
+  
+      const cardsContainer = document.getElementById('cards-container');
+  
+        cards = cards.slice(6,12);
+      cards.forEach(card => {
+          // console.log(card.features)
+          const cardDiv = document.createElement('div')
+          cardDiv.classList.add('col');
+          cardDiv.innerHTML = `
+          <div class="card h-100 p-3">
+              <img src="${card.image}" class="card-img-top p-3" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title fw-bold">Features</h5>
+                  <ol class="card-container">
+                      <li class="${card.features[0] === undefined ? 'd-none' : ''}">${card.features[0]}</li>
+                      <li class="${card.features[1] === undefined ? 'd-none' : ''}">${card.features[1]}</li>
+                      <li class="${card.features[2] === undefined ? 'd-none' : ''}">${card.features[2]}</li>
+                      <li class="${card.features[3] === undefined ? 'd-none' : ''}">${card.features[3]}</li>
+                  </ol>
+              </div>
+              <div class="card-footer bg-white d-flex align-items-center justify-content-between">
+                  <div>
+                      <h5 class="card-title fw-bold">${card.name}</h5>
+                      <p><i class="fa-solid fa-calendar-days"></i>  ${card.published_in}</p>
+                  </div>
+                  <button onclick="loadCardDetails('${card.id}')" class="btn btn-primary rounded-5" data-bs-toggle="modal" data-bs-target="#cardModal"><i class="fa-solid fa-circle-right"></i></button>
+              </div>
+          </div>
+          `;
+          cardsContainer.appendChild(cardDiv);
+          
+      });
+  
+    }
+    
+    // start spinner 
+    toggleSpinner(false);
+    lodeMore.classList.add('d-none');
+
+  })
 
 const loadCardDetails = id => {
     const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`
