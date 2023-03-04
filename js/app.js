e519c630bcc74b72b41258db87190a41621347e4
mohammademon02 <mohammademon0207@gmail.com>
2023-03-04 17:16:06 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ const displayCards = cards => {
         cardDiv.classList.add('col');
         cardDiv.innerHTML = `
         <div class="card h-100 p-3">
-            <img src="${card.image}" class="card-img-top img-fluid" alt="...">
+            <img src="${card.image}" class="card-img-top p-3" alt="...">
             <div class="card-body">
                 <h5 class="card-title fw-bold">Features</h5>
                 <ol class="card-container">
@@ -46,102 +46,77 @@ const loadCardDetails = id => {
     const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => displayCardDetails(data.data))
+    .then(data => console.log(data.data))
 }
 
+
 const displayCardDetails = card => {
+  console.log(card);
 
-    const modalContainer = document.getElementById('modal-container')
-    console.log(card);
-    const modalDiv = document.createElement('div');
-    modalDiv.innerHTML = `
-    <div class="modal fade" id="cardModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="cardModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="cardModalLabel">Modal title</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  const modalContainer = document.getElementById('modal-container');
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+  modalContent.innerHTML = `
+        <div class="modal-header">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+
+        <div class="modal-body d-flex gap-3">
+
+          <div class="card" style="width: 50%;">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+
+              <div class="d-flex gap-3">
+                <div>
+                  <p>price</p>
+                  <p>Basic</p>
                 </div>
-                <div class="d-flex">
+                <div>
+                  <p>price</p>
+                  <p>Pro</p>
+                </div>
+                <div>
+                  <p>price</p>
+                  <p>Enterprise</p>
+                </div>
+              </div>
 
-                  <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                      <h5 class="card-title">${card.description}</h5>
-
-                      <div class="d-flex justify-content-around">
-                        <div>
-                          <p>price</p>
-                          <p>Basic</p>
-                        </div>
-                        <div>
-                          <p>price</p>
-                          <p>Basic</p>
-                        </div>
-                        <div>
-                          <p>price</p>
-                          <p>Basic</p>
-                        </div>
-                      </div>
-
-                      <div class="d-flex justify-content-around">
-                        <div>
-                          <h5 class="card-title">Features</h5>
-                          <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                          </ul>
-                        </div>
-  
-                        <div>
-                          <h5 class="card-title">Integrations</h5>
-                          <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                          </ul>
-                        </div>
-                      </div>
-
-                    </div>
-                  </div>
-
-                  <div class="card" style="width: 18rem;">
-                    <div class="">
-                      <img src=" " class="card-img-top" alt="...">
-                      <button class="btn btn-danger"></button>
-                    </div>
-                    <div class="card-body">
-                      <h3>Hi, how are you doing today?</h3>
-                      <p> </p>
-                    </div>
-                  </div>
-
+              <div class="d-flex gap-3">
+                <div>
+                  <h5>Features</h5>
+                  <ul>
+                    <li></li>
+                  </ul>
+                </div>
+                <div>
+                  <h5>Integrations</h5>
+                  <ul>
+                    <li></li>
+                  </ul>
                 </div>
               </div>
             </div>
           </div>
-    `;
-    modalContainer.appendChild(modalDiv);
+
+          <div class="card" style="width: 50%;">
+            <div>
+              <img src="..." class="card-img-top" alt="...">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Hi, how are you doing today?</h5>
+              <p class="card-text"> </p>
+            </div>
+          </div>
+
+        </div>
+  `;
+
+  modalContainer.appendChild(modalContent);
+
+
 }
 loadCards();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
